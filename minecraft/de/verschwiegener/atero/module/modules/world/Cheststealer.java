bafba0ad39f15b43c9d034cf88714eaf0a287f16
Julius <jlmadrak@web.de>
2021-05-10 19:14:19 +0200

    Revert "AltManager"
    This reverts commit 87f9dac884ec976edd3cfa1465e4a74703f50b6b.

--- minecraft/de/verschwiegener/atero/module/modules/world/Cheststealer.java
+++ minecraft/de/verschwiegener/atero/module/modules/world/Cheststealer.java
@@ -1,4 +1,7 @@
 package de.verschwiegener.atero.module.modules.world;
+
+
+
 import de.verschwiegener.atero.settings.Setting;
 import de.verschwiegener.atero.settings.SettingsItem;
 import net.minecraft.client.Minecraft;
